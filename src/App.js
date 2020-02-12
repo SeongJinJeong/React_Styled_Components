668e8f81ddb041c9d9259787986d3317d76ee2c8
SeongJin <jsj0718tjdwl@naver.com>
2020-02-13 00:18:13 +0900

    Added Animation!!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle, keyframes, css } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body{
@@ -45,12 +45,28 @@ const Button = styled.button`
     outline: none;
   };
   background-color: ${props =>(props.danger ? "red" : "green")};
+  ${props=>{
+    if(props.danger){
+    return css`
+      animation : ${rotation} 2s linear infinite
+    `
+    }
+  }}
 `;
 
 const Anchor = styled(Button)`
   text-decoration: none;
   padding: 10px;
 `;
+
+const rotation = keyframes`
+  from {
+    transform : rotate(0deg)
+  }
+  to {
+    transform : rotate(360deg)
+  }
+`
 
 
 export default App;
